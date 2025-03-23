32f76adf6b3663882f01eaa8e726935205e90cd2
Oskito00 <Oskito00@users.noreply.github.com>
2025-03-23 20:52:30 +0000

    added config changes

--- config.py
+++ config.py
@@ -99,7 +99,9 @@ class DevelopmentConfig(Config):
 
 class ProductionConfig(Config):
     FLASK_ENV = 'production'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://', 1
+    )
 
     #Mail configs
     MAIL_DEFAULT_SENDER = ('MAIL_FROM', 'noreply@ebaymonitor.com')
